00d3c9321d8e4ba68569bf65ca664cec857c40c3
Roman Gusev <groman.me@gmail.com>
2012-10-13 15:41:38 +0400

    add profile_image_url to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  validates :uid, presence: true, uniqueness: true
+
   def self.from_omniauth(auth)
     where(auth.slice('uid')).first || create_from_auth(auth)
   end
@@ -9,6 +11,7 @@ class User < ActiveRecord::Base
       user.uid = auth['uid']
       user.name = auth['info']['name']
       user.nickname = auth['info']['nickname']
+      user.profile_image_url = URI.parse(auth['info']['image']).to_s
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121013054046) do
+ActiveRecord::Schema.define(:version => 20121013110949) do
 
   create_table "decks", :force => true do |t|
     t.string   "url"
@@ -50,8 +50,9 @@ ActiveRecord::Schema.define(:version => 20121013054046) do
     t.string   "uid"
     t.string   "name"
     t.string   "nickname"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+    t.string   "profile_image_url"
   end
 
   add_index "users", ["uid"], :name => "index_users_on_uid"
