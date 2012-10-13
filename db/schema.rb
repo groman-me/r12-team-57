fcac578a7ecc22a0017c77a2b4195e4fd5d27a23
Roman Gusev <groman.me@gmail.com>
2012-10-13 06:04:25 +0400

    update schema

---
+++ db/schema.rb
@@ -0,0 +1,51 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20121013015732) do
+
+  create_table "decks", :force => true do |t|
+    t.string   "url"
+    t.string   "title"
+    t.string   "author"
+    t.integer  "width"
+    t.integer  "height"
+    t.integer  "user_id"
+    t.text     "html"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "decks", ["url"], :name => "index_decks_on_url"
+  add_index "decks", ["user_id"], :name => "index_decks_on_user_id"
+
+  create_table "narrations", :force => true do |t|
+    t.integer  "deck_id"
+    t.integer  "state"
+    t.text     "time_code"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "narrations", ["deck_id", "state"], :name => "index_narrations_on_deck_id_and_state"
+
+  create_table "users", :force => true do |t|
+    t.string   "uid"
+    t.string   "name"
+    t.string   "nickname"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "users", ["uid"], :name => "index_users_on_uid"
+
+end
