fd7ac25661ece84f0c0cbf95052cb9a339558c04
Alex Soulim <soulim@gmail.com>
2012-10-13 11:19:51 +0400

    Move Omniauth config into separate file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
+config = YAML.load_file(File.join(Rails.root, "config", "omniauth.yml"))[Rails.env]
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :twitter, config['twitter']['key'], config['twitter']['secret']
 end
