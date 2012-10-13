e61c2439ba9280a3673a113f8922fe800481750e
Roman Gusev <groman.me@gmail.com>
2012-10-13 04:36:44 +0400

    add empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpeakerVoice::Application.config.secret_token = '24fdf4b80dd9291590c094b451ef7e6e9e686857ef370587cf648ec5a81f23a7663232bdacdde9ba9332a318db59d1b01aa9dddc35e8dbc3b3aa20dce618edb4'
