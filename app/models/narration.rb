8fb23ecb551d659352bc8721f546ea97e3aa83b1
Roman Gusev <groman.me@gmail.com>
2012-10-14 11:41:28 +0400

    fix wav files path

--- app/models/narration.rb
+++ app/models/narration.rb
@@ -2,9 +2,9 @@ class Narration < ActiveRecord::Base
   attr_accessible :deck_id, :time_code, :wav
 
   belongs_to :deck
-  #validates :deck, :presence => true
+  validates :deck, :presence => true
 
-  has_attached_file :wav, storage: :filesystem, path: ':rails_root/files/:url'
+  has_attached_file :wav, storage: :filesystem, path: ':rails_root/files:url'
   has_attached_file :mp3, storage: :filesystem
 
 end
